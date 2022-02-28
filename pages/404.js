571e24ccc945574464ed17a84da3da7306f43cca
rafaeljoia <rafael.joia@fgv.br>
2022-02-28 14:34:55 -0300

    Create project

---
+++ pages/404.js
@@ -0,0 +1,13 @@
+import Link from '../src/components/Link'
+
+export default function HomePage(){
+    return(
+        <div>
+            <h1>Você se perdeu . . .  </h1>
+            <Link href="/">
+                Ir para Home
+            </Link>
+            
+        </div>
+    )
+}
---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+
+export default function FAQPage(){
+    return(
+            <div>
+                <h1>Alura Cases - Pagina de Perguntas - FAQ</h1>
+                <Link href="/" passHref>
+                    <a>Ir para o Home</a>
+                </Link>
+            </div>
+        
+    )
+}
